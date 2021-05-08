dd68c90041c3dddfd571a98ba87b5047d279c3aa
johnjeang <johnjeang@gmail.com>
2021-05-08 07:54:58 -0500

    Initial Commit

---
+++ GroceryManager/GroceryManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  GroceryManagerApp.swift
+//  GroceryManager
+//
+//  Created by John Jeang on 5/8/21.
+//
+
+import SwiftUI
+
+@main
+struct GroceryManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
